b2b495b1b8ecd7a103bd3bee7e451cb5fe92fdea
Wu <wooginawunan@gmail.com>
2023-04-26 20:43:56 -0400

    results analysis

--- notebooks/analysis_round_1.py
+++ notebooks/analysis_round_1.py
@@ -11,9 +11,14 @@ import numpy as np
 
 # Environment variables
 PATH = os.environ['RESULTS_DIR']
-experiments = [ 'Vanilla','single-model-weight-sharing', 'MultiHead', 
-               'MIMO_shuffle_instance', 'MIMO_shuffle_view', 'MIMO_shuffle_all']
-lr = 0.01
+experiments = ['Vanilla/0.1', 'Vanilla/0.1_0', 'Vanilla/0.1_1',
+               'single_model_weight_sharing/0.1', 'single_model_weight_sharing/0.1_0', 'single_model_weight_sharing/0.1_1',
+               'MultiHead/0.1', 'MultiHead/0.1_0', 'MultiHead/0.1_1',
+               'MIMO_shuffle_instance/0.1', 'MIMO_shuffle_instance/0.1_0', 'MIMO_shuffle_instance/0.1_1',
+               'MIMO_shuffle_view/0.1', 'MIMO_shuffle_view/0.1_0', 'MIMO_shuffle_view/0.1_1',
+               'MIMO_shuffle_all/0.1', 'MIMO_shuffle_all/0.1_0', 'MIMO_shuffle_all/0.1_1', 
+               ]
+lr = 0.1
 
 
 ### OVERVIEW: performance and learning curves for the first round of experiments
@@ -21,8 +26,8 @@ lr = 0.01
 
 all_dfs = []
 for exp in experiments:
-    df = pd.read_csv(os.path.join(PATH, exp, str(lr), 'history.csv'))
-    df['model_type'] = exp
+    df = pd.read_csv(os.path.join(PATH, exp, 'history.csv'))
+    df['model_type'], df['seed'] = exp.split('/')
     all_dfs.append(df)
     
 all_dfs = pd.concat(all_dfs)
@@ -36,35 +41,33 @@ sns.set_context("paper",
 
 fig, axs = plt.subplots(2, 2, figsize=(10, 8))
 sns.lineplot(x="epoch", y="loss",
-             hue="model_type",
+             hue="model_type", style="seed", 
              ax = axs[0, 0],
              data=all_dfs)
 
 sns.lineplot(x="epoch", y="test_loss",
-             hue="model_type",
+             hue="model_type", style="seed", 
              ax = axs[0, 1],
              data=all_dfs)
 
 sns.lineplot(x="epoch", y="acc",
-             hue="model_type",
+             hue="model_type", style="seed", 
              ax = axs[1, 0],
              data=all_dfs)
 
 sns.lineplot(x="epoch", y="test_acc",
-             hue="model_type",
+             hue="model_type", style="seed", 
              ax = axs[1, 1],
              data=all_dfs)
 plt.tight_layout()
 plt.savefig('round_1_learning_curves.png')
 # %%
 
-all_dfs.groupby(['model_type'])[['test_acc']].max()
-# %%
+all_dfs.groupby(['model_type', 'seed'])[['test_acc']].max().reset_index().groupby('model_type').agg(['mean', 'std'])
 
 ### Prediction Diversity Analysis 
 
 # %%
-import torch
 import scipy.stats as stats
 import itertools 
 
@@ -86,15 +89,14 @@ def subnetwork_wise_kendalltau(preds_muted):
     return outputs[:, 0] 
 
 
-# %%
 results = {'accuracy_viewwise': [], 
            'accuracy_overall': [], 
            'kendalltau': []}
-for exp in experiments[1:]:
-    predictions = np.load(os.path.join(PATH, exp, str(lr), 'model_best_val_predictions.npy'))
-    labels = np.load(os.path.join(PATH, exp, str(lr), 'model_best_val_labels.npy'))  
+for exp in experiments[3:]:
+    predictions = np.load(os.path.join(PATH, exp, 'model_best_val_predictions.npy'))
+    # labels = np.load(os.path.join(PATH, exp, 'model_best_val_labels.npy'))  
     
-    acc_overall = (np.argmax(predictions.mean(1), 1)==labels).mean()
+    acc_overall = np.equal(np.argmax(predictions.mean(1), 1), labels).mean()
     acc_subnetworks = [(np.argmax(predictions[:, i, :], 1)==labels).mean()\
         for i in range(predictions.shape[1])]
 
@@ -109,12 +111,8 @@ for exp in experiments[1:]:
     taus = subnetwork_wise_kendalltau(preds_muted)
     
     print(f'\tKendall Tau @ Top5 among subnetworks: {taus.mean()}')
-    
-    # print(tau)
 
-    # results['accuracy_viewwise'].append(', '.join([f'{acc:.2f}' for acc in acc_viewwise]))
-    # results['accuracy_overall'].append(acc_overall)
-    # results['kendalltau'].append(tau)
+
 # %%
 
 # results = pd.DataFrame(results, index=experiments)
@@ -148,8 +146,8 @@ for exp in experiments[1:]:
 # %%
 for exp in experiments:
     predictions = np.load(os.path.join(
-        PATH, exp, str(lr), 'model_best_val_predictions_robustness.npy'))
-    labels = np.load(os.path.join(PATH, exp, str(lr), 'model_best_val_labels.npy')) 
+        PATH, exp, 'model_best_val_predictions_robustness.npy'))
+    #labels = np.load(os.path.join(PATH, exp, 'model_best_val_labels.npy')) 
 
     num_views = predictions.shape[0]
     acc_per_view = [(np.argmax(predictions[i, :, :, :].mean(1), 1)==labels).mean()\
@@ -160,13 +158,13 @@ for exp in experiments:
     for i in range(num_views):
         print(f'\tAccuract when missing view {i}: {acc_per_view[i]}')
         
-        if exp == 'Vanilla':
+        if 'Vanilla' in exp:
             continue
         else:
             top=5
             preds_muted = [trunk_pred_top(predictions[i, :, j, :], 
                                         labels, top, mute_true=True)\
-                for j in range(num_views-1 if exp=='single-model-weight-sharing'\
+                for j in range(num_views-1 if 'single_model_weight_sharing' in exp\
                     else num_views)]
             taus = subnetwork_wise_kendalltau(preds_muted)
             print(f'\t\tKendall Tau @ Top5 among subnetworks: {taus.mean()}')
